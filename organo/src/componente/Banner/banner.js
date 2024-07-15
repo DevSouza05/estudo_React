f2c10234da63b477da8b78aaed9388ad1978c710
peterson.moraes <peterson.moraes@PC-SUPORTE>
2024-07-15 12:48:27 -0300

    add banner

--- organo/src/componente/Banner/banner.js
+++ organo/src/componente/Banner/banner.js
@@ -1,9 +1,14 @@
 "use strict"
 
+import './banner.css'
+
 function Banner(){
     //JSX
     return(
-        <img src="./imagens/banner.png" a="O banner principal da página do Organo"></img>
+      <header className="banner">
+          <img src="./imagens/banner.png" a="O banner principal da página do Organo"></img>
+      </header>
+
     )
 }
 
