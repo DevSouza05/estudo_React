5d83a05e1e74cbcc2b4a5386ea1dc5df7e4cbac5
peterson.moraes <peterson.moraes@PC-SUPORTE>
2024-07-15 11:12:04 -0300

    settings for first project React

---
+++ organo/src/componente/Banner/banner.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function Banner(){
+    //JSX
+    return(
+        <img src="./imagens/banner.png" a="O banner principal da página do Organo"></img>
+    )
+}
+
+export default Banner
